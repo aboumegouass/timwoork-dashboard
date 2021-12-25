d9a8ba4c5648d071d23f0730ad4af98440427d4e
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2021-12-25 20:36:10 +0100

    Edit Some Bug

--- store/auth/authActions.tsx
+++ store/auth/authActions.tsx
@@ -15,61 +15,6 @@ import Cookies from 'js-cookie'
 import router from "next/router";
 import useSWR from 'swr'
 
-export const addNewProduct = () => {
-    return async (dispatch: CallableFunction) => {
-        try {
-            const token = Cookies.get('token')
-            dispatch({
-                type: types.ADD_PRODUCT_LOADING,
-            });
-            const res = await API.post("api/product/store", {}, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            // If Activate Network 
-            // Authentication was successful.
-            if (res.status == 201 || res.status == 200) {
-                dispatch({
-                    type: types.ADDED_PRODUCT,
-                });
-                router.push({
-                    pathname: `/add-new/overview`,
-                    query: {
-                        id: res.data.data.id, // pass the id 
-                    },
-                })
-            } else {
-                return dispatch({
-                    type: types.ADD_NEW_PRODUCT_ERROR,
-                    payload: "حدث خطأ غير متوقع",
-                });
-            }
-        } catch (ex) {
-            if (ex.response && ex.response.status === 403) {
-                return dispatch({
-                    type: types.ADD_NEW_PRODUCT_ERROR,
-                    payload: "هذا العنصر غير موجود",
-                });
-            }
-
-            if (ex.response && ex.response.status === 422) {
-                return dispatch({
-                    type: types.ADD_NEW_PRODUCT_ERROR,
-                    payload: "يرجى ملأ البيانات",
-                });
-            }
-
-            if (ex.response && ex.response.status === 401) {
-                return dispatch({
-                    type: types.ADD_NEW_PRODUCT_ERROR,
-                    payload: "ليس لديك الصلاحية",
-                });
-            }
-        }
-    }
-}
-
 /**
  * Load currently logged in user from DB.
  *
@@ -115,22 +60,18 @@ export const login = (username: string, password: string): any => {
                     type: types.LOGIN_SUCCESS,
                 });
                 Cookies.set('token', res.data.data.token)
-                if (res.data.data.is_verified) {
-                    switch (res.data.data.step) {
-                        case 0:
-                            router.push('/user/personalInformations')
-                            break;
-                        case 1:
-                            router.push('/user/personalInformations')
-                            break;
-                        case 2:
-                            router.push('/user/numberPhone')
-                            break;
-                        default:
-                            router.push('/')
-                    }
-                } else {
-                    router.push('/emailConfig')
+                switch (res.data.data.step) {
+                    case 0:
+                        router.push('/user/personalInformations')
+                        break;
+                    case 1:
+                        router.push('/user/personalInformations')
+                        break;
+                    case 2:
+                        router.push('/user/numberPhone')
+                        break;
+                    default:
+                        router.push('/')
                 }
             }
         } catch (error: any) {
@@ -150,75 +91,6 @@ export const login = (username: string, password: string): any => {
                 return dispatch({
                     type: types.LOGIN_ERROR,
                     payload: "حدث خطأ.. يرجى التأكد من البيانات",
-                });
-            } else {
-                return dispatch({
-                    type: types.LOGIN_ERROR,
-                    payload: "حدث خطأ غير متوقع",
-                });
-            }
-        }
-    };
-};
-/**
- * Register functionality.
- *
- * @param {string} email
- *   The username of the user.
- * @param {string} password
- *   The password of the user.
- */
-export const register = (email: string, password: string): any => {
-    return async (dispatch: CallableFunction) => {
-        try {
-            // Start loading.
-            dispatch({ type: types.START_REGISTER_LOADING });
-
-            const res = await API.post("api/register", {
-                email,
-                password,
-            })
-            // Authentication was successful.
-            if (res.status === 200) {
-                dispatch({
-                    type: types.REGISTER_SUCCESS,
-                });
-                Cookies.set('token', res.data.data.token)
-                if (res.data.data.is_verified) {
-                    switch (res.data.data.step) {
-                        case 0:
-                            router.push('/user/personalInformations')
-                            break;
-                        case 1:
-                            router.push('/user/personalInformations')
-                            break;
-                        case 2:
-                            router.push('/user/numberPhone')
-                            break;
-                        default:
-                            router.push('/')
-                    }
-                } else {
-                    router.push('/emailConfig')
-                }
-            }
-        } catch (error: any) {
-            if (error.response && error.response.status === 422) {
-                return dispatch({
-                    type: types.REGISTER_ERROR,
-                    payload: "يرجى تعبئة البيانات",
-                });
-            }
-            if (error.response && error.response.status === 419) {
-                return dispatch({
-                    type: types.REGISTER_ERROR,
-                    payload: "العملية غير ناجحة",
-                });
-            }
-            if (error.response && error.response.status === 400) {
-                return dispatch({
-                    type: types.REGISTER_ERROR,
-                    payload: "حدث خطأ ما لم يتم العثور على رمز التفعيل الخاص بك",
                 });
             } else {
                 return dispatch({
